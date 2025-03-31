4731756df36ce50c992aa8bc7861a71ecb04a737
Rasul100 <159882673+Rasul100@users.noreply.github.com>
2025-03-31 17:03:47 +0300

    Create fuzzy_search.py

---
+++ project/app/services/fuzzy_search.py
@@ -0,0 +1,36 @@
+def levenshtein(s1: str, s2: str) -> int:
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+    if len(s2) == 0:
+        return len(s1)
+    previous_row = list(range(len(s2) + 1))
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1
+            deletions = current_row[j] + 1
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+    return previous_row[-1]
+
+def damerau_levenshtein(s1: str, s2: str) -> int:
+    d = {}
+    lenstr1 = len(s1)
+    lenstr2 = len(s2)
+    for i in range(-1, lenstr1 + 1):
+        d[(i, -1)] = i + 1
+    for j in range(-1, lenstr2 + 1):
+        d[(-1, j)] = j + 1
+    for i in range(lenstr1):
+        for j in range(lenstr2):
+            cost = 0 if s1[i] == s2[j] else 1
+            d[(i, j)] = min(
+                d[(i - 1, j)] + 1,           
+                d[(i, j - 1)] + 1,           
+                d[(i - 1, j - 1)] + cost     
+            )
+            if i and j and s1[i] == s2[j - 1] and s1[i - 1] == s2[j]:
+                d[(i, j)] = min(d[(i, j)], d[(i - 2, j - 2)] + cost)  
+    return d[(lenstr1 - 1, lenstr2 - 1)]
+
